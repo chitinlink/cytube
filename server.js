27c1125af0a35fab3245a160a860705f776ea46c
Ada <interimgoliath@gmail.com>
2021-03-31 04:29:15 +0000

    shrug?

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const WebSocketServer = require("ws").Server;
+const
+  WebSocketServer = require("ws").Server,
+  http = require("http"),
+  express = require("express"),
+  app = express(),
+  port = process.env.PORT || 5000;
 
-const WEBSOCKET_PORT = 8080;
+app.use(express.static(__dirname + "/"))
+const server = http.createServer(app)
+server.listen(port)
 
-const server = new WebSocketServer({ port: WEBSOCKET_PORT });
+const server = new WebSocketServer({ server });
 let sockets = [];
 
 server.on("connection", socket => {
