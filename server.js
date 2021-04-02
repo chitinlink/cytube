7ee6b6e881010b3f27a5459333eee0b1961596cf
Ada <interimgoliath@gmail.com>
2021-04-02 14:18:37 +0000

    test

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const
   WebSocketServer = require("ws").Server,
-  http = require("http"),
-  express = require("express"),
-  app = express(),
   port = process.env.PORT || 5000;
-
-app.use(express.static(__dirname + "/"))
-const server = http.createServer(app)
-server.listen(port)
 
 const server = new WebSocketServer({ server });
 let sockets = [];
