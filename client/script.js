018c3f852d4f102127e4f6cd536dda6b74145efa
Ada <interimgoliath@gmail.com>
2021-04-06 08:28:58 +0000

    improvementssss

--- client/script.js
+++ client/script.js
@@ -9,17 +9,20 @@
 // "Drink calls" permission. I recommend setting that to channel admin only.
 
 const
-  WEBSOCKET_ADDR = "wss://your_server_here",
+  WEBSOCKET_ADDR = "wss://tabyss-cytube.herokuapp.com",
   WEBSOCKET_HEARTBEAT = 1000,
   WEBSOCKET_TIMEOUT = 30000 + WEBSOCKET_HEARTBEAT;
 
 /* -------------------------------------------------------------------------- */
 
 const videowrap = document.getElementById("ytapiplayer").parentElement;
-let socket, state, admin_key;
+let _socket, admin_key;
+let initial_state_received = false;
+let is_op = false;
+let state = {};
 
 // Utils
-const createCutout = (id, image_url) => {
+const createCutout = (image_url) => {
   let
     c = document.createElement("div"),
     e = document.createElement("img");
@@ -69,7 +72,6 @@ const setAspectRatio = ratio => {
       .replace("embed-responsive-16by9", "embed-responsive-4by3");
   } else return console.error("Unknown aspect ratio");
 
-
   // // Doing this makes the chat resize to match
   // // this changes the video size if it's already maxed, though
   // CyTube.ui.changeVideoWidth(1);
@@ -81,25 +83,77 @@ const setAspectRatio = ratio => {
   sendState();
 };
 
+const setFlipH = phase => {
+  let e = document.getElementById("eff_flipH");
+  if (phase) { e.classList.add("on"); }
+  else e.classList.remove("on");
+  if (!state.hasOwnProperty("effects")) state["effects"] = {};
+  if (!state["effects"].hasOwnProperty("flipH")) state["flipH"] = false;
+  state["effects"]["flipH"] = phase;
+  sendState();
+};
+
+const setFlipV = phase => {
+  let e = document.getElementById("eff_flipV");
+  if (phase) { e.classList.add("on"); }
+  else e.classList.remove("on");
+  if (!state.hasOwnProperty("effects")) state["effects"] = {};
+  if (!state["effects"].hasOwnProperty("flipV")) state["flipV"] = false;
+  state["effects"]["flipV"] = phase;
+  sendState();
+};
+
 const initState = () => {
-  let aspect_ratio = (document.querySelector("#videowrap .embed-responsive").classList.contains("embed-responsive-16by9") ? "16by9" : "4by3");
+  console.log("Initializing state.", state);
+  // let aspect_ratio = (document.querySelector("#videowrap .embed-responsive").classList.contains("embed-responsive-16by9") ? "16by9" : "4by3");
 
   state = {
     "aspect_ratio": aspect_ratio,
+    "effects": {},
     "cutouts": []
   };
-}
+};
 
 const receiveState = state => {
+
+  if (!state.hasOwnProperty("aspect_ratio")) state["aspect_ratio"] = "16by9";
   setAspectRatio(state["aspect_ratio"]);
+
+  if (!state.hasOwnProperty("effects")) state["effects"] = {};
+  if (!state["effects"].hasOwnProperty("flipH")) state["flipH"] = false;
+  setFlipH(state["effects"]["flipH"]);
+  if (!state["effects"].hasOwnProperty("flipV")) state["flipV"] = false;
+  setFlipV(state["effects"]["flipV"]);
+
+  if (!state.hasOwnProperty("cutouts")) state["cutouts"] = [];
 };
 
 const sendState = () => {
-  console.info("Sending state:", state);
-  socket.send(JSON.stringify({ key: admin_key, state }))
+  if (!is_op) return;
+  console.info("WS - Sending state:");
+  console.dir(state);
+  _socket.send(JSON.stringify({ key: admin_key, state }));
 };
 
 // UI
+// General UI manipulation
+const setupUI = () => {
+  // Effect wrappers
+  let
+    flipH = document.createElement("div"),
+    flipV = document.createElement("div");
+  flipH.id = "eff_flipH";
+  flipV.id = "eff_flipV";
+  flipH.appendChild(flipV);
+  videowrap.parentNode.appendChild(flipH);
+  flipV.appendChild(videowrap);
+
+  // Cutouts
+  let cutouts = document.createElement("div");
+  cutouts.id = "cutouts";
+  videowrap.appendChild(cutouts);
+}
+// Admin panel
 const setupAdminPanel = () => {
   // Add context for CSS styles
   document.body.classList.add("admin");
@@ -113,16 +167,18 @@ const setupAdminPanel = () => {
   let adminkeyform = document.createElement("form");
   let adminkeyinput = document.createElement("input");
   adminkeyinput.type = "text";
-  adminkeyinput.placeholder = "Admin key"
+  adminkeyinput.placeholder = "Admin key";
   panel.appendChild(adminkeyform);
   adminkeyform.appendChild(adminkeyinput);
 
   // You're supposed to enter the admin key, and then hit Enter
   adminkeyform.addEventListener("submit", e => {
     e.preventDefault();
-    // Then we hide the text input,
+    // Hide the text input
     adminkeyform.hidden = true;
     document.getElementById("admin-controls").disabled = false;
+    // Update aspect ratio select
+    document.getElementById("admin-aspectratio").value = state["aspect_ratio"];
     // Set the admin key to whatever you typed in
     admin_key = adminkeyinput.value;
     // And broadcast your state to everyone else
@@ -143,19 +199,32 @@ const setupAdminPanel = () => {
     .addEventListener("change", e => setAspectRatio(e.target.value));
 
   // Effects
-  // fieldset.appendChild(createButton("flipH", "FlipH"));
-  // document.getElementById("flipH")
-  //   .addEventListener("click", toggleFlipH);
-  // fieldset.appendChild(createButton("flipV", "FlipV"));
-  // document.getElementById("flipV")
-  //   .addEventListener("click", toggleFlipV);
-}
+  fieldset.appendChild(createButton("flipH", "FlipH"));
+  document.getElementById("flipH")
+    .addEventListener("click", () => setFlipH(!state["effects"]["flipH"]));
+  fieldset.appendChild(createButton("flipV", "FlipV"));
+  document.getElementById("flipV")
+    .addEventListener("click", () => setFlipV(!state["effects"]["flipV"]));
+
+  // Cutouts
+  let cutout_form = document.createElement("form")
+  fieldset.appendChild(cutout_form);
+  let cutout_url = document.createElement("input");
+  cutout_url.type = "url";
+  cutout_url.id = "cutout_url";
+  cutout_url.placeholder = "Image URL";
+  cutout_form.appendChild(cutout_url);
+  cutout_url.addEventListener("submit", e => {
+    e.preventDefault();
+    createCutout(cutout.url.value);
+  });
+};
 
 // Websocket stuff below.
 // Whenever the websocket receives data (other than a heartbeat), it passes
 // it to receiveState.
 const connect = () => {
-  socket = new WebSocket(WEBSOCKET_ADDR);
+  _socket = new WebSocket(WEBSOCKET_ADDR);
   let
     connected = false,
     pingTimeout;
@@ -165,21 +234,30 @@ const connect = () => {
     clearTimeout(pingTimeout);
     pingTimeout = setTimeout(socket.close, WEBSOCKET_TIMEOUT);
 
-    setTimeout(() => socket.send("pong"), WEBSOCKET_HEARTBEAT);
-  }
+    setTimeout(() => _socket.send("pong"), WEBSOCKET_HEARTBEAT);
+  };
 
   // Receive message
-  socket.addEventListener("message", e => {
+  _socket.addEventListener("message", e => {
     // Heartbeat
     if (e.data === "ping") return heartbeat();
 
     console.info("WS - Message:", e.data);
 
-    receiveState(JSON.parse(e.data));
+    let new_state = JSON.parse(e.data);
+
+    receiveState(new_state);
+
+    if (!initial_state_received) {
+      initial_state_received = true;
+      if (Object.entries(new_state).length !== 0) { state = new_state; }
+      else initState();
+      if (is_op) setupAdminPanel();
+    }
   });
 
   // Connect
-  socket.addEventListener("open", () => {
+  _socket.addEventListener("open", () => {
     console.info("WS - Connected");
     serverMessage("reconnect", "Connected to websocket");
     connected = true;
@@ -187,7 +265,7 @@ const connect = () => {
   });
 
   // Disconnect
-  socket.addEventListener("close", () => {
+  _socket.addEventListener("close", () => {
     if (connected) {
       console.info("WS - Disconnected");
       serverMessage("disconnect", "Disconnected from websocket");
@@ -198,16 +276,15 @@ const connect = () => {
 };
 
 // Maybe overdoing it?
-
 const do_setup = () => {
-  const OP = hasPermission("drink");
+  setupUI();
+
+  is_op = hasPermission("drink");
   // FIXME sometimes it's false when it should be true, probably because this is loading at the wrong time
-  console.debug("OP", OP);
+  console.debug("OP", is_op);
 
   connect();
-  initState();
-  if (OP) setupAdminPanel();
 };
 
-if (document.readyState === "complete") { do_setup() }
+if (document.readyState === "complete") { do_setup(); }
 else window.addEventListener("load", do_setup);
